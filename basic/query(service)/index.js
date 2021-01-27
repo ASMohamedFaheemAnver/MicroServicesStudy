a6ad45b8ec154bc0ec21bb5aa892be7aa9b59c31
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-01-27 12:35:56 +0530

    Syncing events whenever server missed past events,

--- basic/query(service)/index.js
+++ basic/query(service)/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-  console.log({ event: req.body });
-  const { type, data } = req.body;
+const handleEvents = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -31,10 +26,29 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
 
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  console.log({ event: req.body });
+  const { type, data } = req.body;
+  handleEvents(type, data);
   res.send({ msg: "200K OK!" });
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("server(post with comments) is listenning on port : 4002!");
+  const res = await axios.get("http://localhost:4005/events");
+
+  for (let event of res.data) {
+    console.log({
+      processing: {
+        event: event,
+      },
+    });
+    handleEvents(event.type, event.data);
+  }
 });
