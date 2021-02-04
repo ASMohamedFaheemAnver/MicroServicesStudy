5216e71462593ce2f978be3c0b76723ec1fa40e6
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-02-04 19:19:08 +0530

    Managing all deployment using skaffold,

--- basic/client/src/App.js
+++ basic/client/src/App.js
@@ -5,10 +5,9 @@ import PostList from "./PostList";
 function App() {
   return (
     <div className="container">
-      <h1>Create post</h1>
+      <h1>Create post!</h1>
       <PostCreate></PostCreate>
       <hr></hr>
-      <h1>Created posts</h1>
       <PostList></PostList>
     </div>
   );
--- basic/client/src/PostList.js
+++ basic/client/src/PostList.js
@@ -27,7 +27,12 @@ const PostList = () => {
     );
   });
 
-  return <div className="d-flex flex-row flex-wrap justify-content-between ">{renderedPosts}</div>;
+  return (
+    <>
+      {Object.values(posts).length ? <h1>Created posts</h1> : ""}
+      <div className="d-flex flex-row flex-wrap justify-content-between ">{renderedPosts}</div>
+    </>
+  );
 };
 
 export default PostList;
