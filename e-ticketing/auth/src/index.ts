fc4bfc5778b98fbc580a0bdb4875ef7cdaf5dd38
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-02-12 11:36:55 +0530

    Ingress loadbalancing config,

--- e-ticketing/auth/src/index.ts
+++ e-ticketing/auth/src/index.ts
@@ -4,6 +4,21 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/", (req, res) => {
+  res.json({
+    msg: "404, Route not found!",
+    meta: {
+      developer: "A.S. Mohamed Faheem Anver",
+      social_media: {
+        facebook: "https://www.facebook.com/jstr.faheemanver/",
+        linkedin: "https://www.linkedin.com/in/abdul-saleem-mohamed-faheem/",
+        github: "https://github.com/asmohamedfaheemanver",
+        website: "https://mohamedfaheem.netlify.app/",
+      },
+    },
+  });
+});
+
 app.listen(3000, () => {
   console.log("server listening on port 3000!");
 });
