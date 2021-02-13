059992260f30f8d07e5f7284c376079cf890952b
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-02-14 00:33:49 +0530

    Routes,

--- e-ticketing/auth/src/index.ts
+++ e-ticketing/auth/src/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import { json } from "body-parser";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
 const app = express();
 app.use(json());
 
-app.get("/", (req, res) => {
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.use((req, res) => {
   res.json({
     msg: "404, Route not found!",
     meta: {
