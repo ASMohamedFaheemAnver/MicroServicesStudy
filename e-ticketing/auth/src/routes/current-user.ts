8f666e3982bfddb5e8963e4cb8a0b451f359ff4c
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-02-22 16:00:11 +0530

    Signin and signout hit point,

--- e-ticketing/auth/src/routes/current-user.ts
+++ e-ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/current-user", (req, res) => {
-  res.json({ msg: "/api/users/current-user" });
+  if (!req.session?.jwt) {
+    return res.json({ message: "invalid token" });
+  }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ message: "valid token", currentUser: payload });
+  } catch (err) {
+    return res.json({ message: "invalid token" });
+  }
 });
 
 export { router as currentUserRouter };
