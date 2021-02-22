a99729e0a87eeed78e284a17e5fbf20f58a5e1cb
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-02-22 18:31:42 +0530

    Signup tests,

--- e-ticketing/auth/src/test/setup.ts
+++ e-ticketing/auth/src/test/setup.ts
@@ -5,6 +5,7 @@ import { app } from "../app";
 let mongo: any;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = "testing";
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
   await mongoose.connect(mongoUri, {
