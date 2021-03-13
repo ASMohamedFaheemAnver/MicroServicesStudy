964b9fc897236a3e31ee4edc08b7fdc69c976e3f
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-03-13 21:12:39 +0530

    Queue group, manual ack and handling client shutdown,

--- e-ticketing/nats-test/src/listener.ts
+++ e-ticketing/nats-test/src/listener.ts
@@ -1,13 +1,26 @@
 import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
-const stan = nats.connect("ticketing", "listenerId", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
-  const subscription = stan.subscribe("ticket:created");
+
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    options
+  );
   subscription.on("message", (msg: Message) => {
+    stan.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+
     const data = msg.getData();
     if (typeof data == "string") {
       console.log(
@@ -16,5 +29,14 @@ stan.on("connect", () => {
         )}`
       );
     }
+    msg.ack();
   });
+});
+
+process.on("SIGINT", () => {
+  stan.close();
+});
+
+process.on("SIGTERM", () => {
+  stan.close();
 });
