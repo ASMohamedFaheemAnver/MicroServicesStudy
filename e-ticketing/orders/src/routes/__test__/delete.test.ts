de721e36d027ab6db4ee55b5c293f1a9a004f550
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-03-20 21:34:25 +0530

    Testing event emitting between services,

--- e-ticketing/orders/src/routes/__test__/delete.test.ts
+++ e-ticketing/orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 import { Order, OrderStatus } from "../../models/order";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("marks an order as cancelled", async () => {
   const ticket = Ticket.build({ title: "concert", price: 20 });
@@ -23,4 +24,21 @@ it("marks an order as cancelled", async () => {
   expect(updatedOrder.status).toEqual(OrderStatus.CANCELLED);
 });
 
-it.todo("emits a order cancelled event");
+it("emits a order cancelled event", async () => {
+  const ticket = Ticket.build({ title: "concert", price: 20 });
+  await ticket.save();
+
+  const cookie = global.signin();
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", cookie)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", cookie)
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
