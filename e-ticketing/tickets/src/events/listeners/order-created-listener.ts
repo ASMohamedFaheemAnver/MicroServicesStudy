8c1df7d395bb083065c08df47468625ea19905f2
FreeDoM <jstrfaheem065@gmail.com>
2021-05-03 23:12:15 +0530

    Emitting order update event,

--- e-ticketing/tickets/src/events/listeners/order-created-listener.ts
+++ e-ticketing/tickets/src/events/listeners/order-created-listener.ts
@@ -7,6 +7,7 @@ import {
 } from "@coders2authority/tik-common";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../model/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 import { queueGroupName } from "./queue-group-name";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
@@ -29,6 +30,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     }
     ticket.set({ orderId: data.id });
     await ticket.save();
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      version: ticket.version,
+      orderId: ticket.orderId,
+    });
+
     msg.ack();
   }
 }
--- e-ticketing/tik-common/src/events/order-created-event.ts
+++ e-ticketing/tik-common/src/events/order-created-event.ts
@@ -5,7 +5,7 @@ export interface OrderCreatedEvent {
   subject: Subjects.ORDER_CREATED;
   data: {
     id: string;
-    version: string;
+    version: number;
     status: OrderStatus;
     userId: string;
     expiresAt: string;
