4ef7d7493e04a50a0d61a3db2eacf4d79344f6a1
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-07-14 17:24:39 +0530

    Listening to order creation,

---
+++ e-ticketing/payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,35 @@
+import {
+  Listener,
+  OrderCreatedEvent,
+  OrderStatus,
+  Subjects,
+} from "@coders2authority/tik-common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../model/order";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.ORDER_CREATED = Subjects.ORDER_CREATED;
+  queueGroupName: string = queueGroupName;
+  async onMessage(
+    data: {
+      id: string;
+      version: number;
+      status: OrderStatus;
+      userId: string;
+      expiresAt: string;
+      ticket: { id: string; price: number };
+    },
+    msg: Message
+  ) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version,
+    });
+    await order.save();
+    msg.ack();
+  }
+}
