24c2a4c7b635f1328b6b723c719f60f51d414a54
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-07-17 01:14:11 +0530

    Testing payment route,

--- e-ticketing/payments/src/routes/__test__/new.test.ts
+++ e-ticketing/payments/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import mongoose from "mongoose";
 import { app } from "../../app";
+import { Order, OrderStatus } from "../../model/order";
 
 it("returns a 404 when purchasing an order that doen't exist", async () => {
   const res = await request(app)
@@ -13,5 +14,47 @@ it("returns a 404 when purchasing an order that doen't exist", async () => {
 
   expect(res.status).toEqual(404);
 });
-it("returns a 401 when purchasing an order that doesn't belong to the user", async () => {});
-it("returns a 400 when purchasing a cancelled order", async () => {});
+
+it("returns a 401 when purchasing an order that doesn't belong to the user", async () => {
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    userId: mongoose.Types.ObjectId().toHexString(),
+    price: 20,
+    status: OrderStatus.CREATED,
+  });
+
+  await order.save();
+
+  const res = await request(app)
+    .post("/api/payments")
+    .send({
+      token: "token",
+      orderId: order.id,
+    })
+    .set("Cookie", globalThis.signin());
+
+  expect(res.status).toEqual(401);
+});
+
+it("returns a 400 when purchasing a cancelled order", async () => {
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    userId: mongoose.Types.ObjectId().toHexString(),
+    price: 20,
+    status: OrderStatus.CANCELLED,
+  });
+
+  await order.save();
+
+  const res = await request(app)
+    .post("/api/payments")
+    .send({
+      token: "token",
+      orderId: order.id,
+    })
+    .set("Cookie", globalThis.signin(order.userId));
+
+  expect(res.status).toEqual(400);
+});
--- e-ticketing/payments/src/test/setup.ts
+++ e-ticketing/payments/src/test/setup.ts
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 import { app } from "../app";
 
 declare global {
-  var signin: () => string[];
+  var signin: (id?: string) => string[];
 }
 
 jest.mock("../nats-wrapper");
@@ -36,10 +36,10 @@ afterAll(async () => {
   await mongoose.disconnect();
 });
 
-globalThis.signin = () => {
+globalThis.signin = (id?: string) => {
   // build a jwt payload {id, email}
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
+    id: id || new mongoose.Types.ObjectId().toHexString(),
     email: "test@test.com",
   };
 
